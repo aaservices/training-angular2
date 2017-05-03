57cb960cbd0b8dc2c333373f8539764834d45406
Christina Kaskoura <christina.kaskoura@agileactors.com>
2017-05-03 21:39:37 +0300

    Added editorconfig and formatted source files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,19 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+    selector: 'my-app',
+    template: `<h1>Hello {{name}}</h1>`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent { name = 'Angular'; }
+
+class Greeter {
+    greeting: string;
+
+    constructor(message: string) {
+        this.greeting = message;
+    }
+
+    greet(): string {
+        return 'Hello, ' + this.greeting;
+    }
+}
