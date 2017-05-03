59f8998518335a6ca93984a195e037f0d3b56c02
Christina Kaskoura <christina.kaskoura@agileactors.com>
2017-05-04 00:23:59 +0300

    Removed sample code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,15 +5,3 @@ import { Component } from '@angular/core';
     template: `<h1>Hello {{name}}</h1>`,
 })
 export class AppComponent { name = 'Angular'; }
-
-class Greeter {
-    greeting: string;
-
-    constructor(message: string) {
-        this.greeting = message;
-    }
-
-    greet(): string {
-        return 'Hello, ' + this.greeting;
-    }
-}
