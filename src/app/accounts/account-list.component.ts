739b97883084468c0e7f62010996351c2d716864
Christina Kaskoura <christina.kaskoura@agileactors.com>
2017-05-18 18:22:33 +0300

    Created account list component

---
+++ src/app/accounts/account-list.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'account-list',
+    templateUrl: 'app/accounts/account-list.component.html'
+})
+export class AccountListComponent { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AccountListComponent } from './accounts/account-list.component';
 
 @NgModule({
     imports: [BrowserModule],
-    declarations: [AppComponent],
+    declarations: [AppComponent, AccountListComponent],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
