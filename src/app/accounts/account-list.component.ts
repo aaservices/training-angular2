01a7322c0998801ed9df52149e25c3840eeccf04
Stelios Charmpalis <stelioschar@gmail.com>
2017-05-21 12:38:46 +0300

    add Account class and extend the AccountListComponent

--- src/app/accounts/account-list.component.ts
+++ src/app/accounts/account-list.component.ts
@@ -1,7 +1,13 @@
 import { Component } from '@angular/core';
+import { Account } from './account';
 
 @Component({
     selector: 'account-list',
     templateUrl: 'app/accounts/account-list.component.html'
 })
-export class AccountListComponent { }
+export class AccountListComponent extends Account {
+    constructor() {
+        super(4);
+        console.log(this.getId());
+    }
+}
