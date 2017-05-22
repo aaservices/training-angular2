d5c08389b24b8d8f64357662cf615374233d844a
Christina Kaskoura <christina.kaskoura@gmail.com>
2017-05-22 21:20:16 +0300

    Added acount type and account filtering using search field and search button

--- src/app/accounts/account-list.component.ts
+++ src/app/accounts/account-list.component.ts
@@ -1,30 +1,42 @@
 import { Component } from '@angular/core';
+import { Account } from './account.type';
 
 @Component({
     selector: 'account-list',
     templateUrl: 'app/accounts/account-list.component.html'
 })
 export class AccountListComponent {
-    private accounts: Array<{ name: string, price: string }>;
+    private accounts: Array<Account>;
+    private viewAccounts: Array<Account>;
     private searchTerm: string;
     private listVisibility: boolean;
 
     constructor() {
         this.accounts = [
-            {
-                name: 'Savings account',
-                price: '23'
-            },
-            {
-                name: 'Current account',
-                price: '54'
-            }
+            new Account('Savings account', 300),
+            new Account('Current account', 500, 'Work expenses')
         ];
+        this.viewAccounts = this.accounts;
 
         this.listVisibility = true;
     }
 
     toggleList(): void {
         this.listVisibility = !this.listVisibility;
+    }
+
+    filterAccounts(): void {
+        if (this.searchTerm) {
+            this.viewAccounts = this.accounts.filter((element: Account) => {
+                return element.name.indexOf(this.searchTerm) >= 0;
+            });
+        } else {
+            this.viewAccounts = this.accounts;
+        }
+    }
+
+    clearSearch(): void {
+        this.searchTerm = '';
+        this.viewAccounts = this.accounts;
     }
 }
---
+++ src/app/accounts/account.type.ts
@@ -0,0 +1,5 @@
+export class Account {
+    constructor(public name: string,
+                public amount: number,
+                public friendlyName?: string, ) {}
+}
