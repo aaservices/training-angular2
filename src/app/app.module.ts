744ea491f5240f5f57319887f612c6862a5f417f
Stelios Charmpalis <stelioschar@gmail.com>
2017-05-26 22:44:07 +0300

    import account-list-item to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AccountListComponent } from './accounts/account-list.component';
+import { AccountListItemComponent } from './accounts/account-list-item/account-list-item';
 import { AccountFilterPipe } from './accounts/account-filter.pipe';
 
 @NgModule({
@@ -11,6 +12,7 @@ import { AccountFilterPipe } from './accounts/account-filter.pipe';
     declarations: [
         AppComponent,
         AccountListComponent,
+        AccountListItemComponent,
         AccountFilterPipe
     ],
     bootstrap: [AppComponent]
