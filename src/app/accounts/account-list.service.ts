e0f9158cdce6bc32c63e8c6159c98d2a193f155a
llkoun <leandros.kounadis@camelotglobal.com>
2017-06-07 12:01:36 +0300

    added injectable decorator

--- src/app/accounts/account-list.service.ts
+++ src/app/accounts/account-list.service.ts
@@ -1,6 +1,8 @@
+import {Injectable} from '@angular/core';
 import {ACCOUNTS} from './mock-accounts';
 import {Logger} from '../logger.service';
 
+@Injectable()
 export class AccountListService {
 
     constructor(private logger:Logger){}
