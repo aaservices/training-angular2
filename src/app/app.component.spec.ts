b853ee83ad85c1ff09d3205ff7d9d3746325314b
Christina Kaskoura <christina.kaskoura@gmail.com>
2017-06-08 22:47:33 +0300

    Added mock AccountList component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,13 @@
 import { ComponentFixture, TestBed, ComponentFixtureAutoDetect, async } from '@angular/core/testing';
 import { By }              from '@angular/platform-browser';
-import { DebugElement }    from '@angular/core';
+import { DebugElement, Component }    from '@angular/core';
 import { AppComponent } from './app.component';
+
+@Component({
+    selector: 'account-list',
+    template: `This is a test`
+})
+class TestAccountListComponent {}
 
 describe('AppComponent', () => {
     let component: AppComponent;
@@ -11,24 +17,18 @@ describe('AppComponent', () => {
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [ AppComponent ], // declare the test component
+            declarations: [ AppComponent, TestAccountListComponent ], // declare the test component
             providers: [{provide: ComponentFixtureAutoDetect, useValue: true}]
         }).compileComponents();
     }));
 
     beforeEach(() => {
-        // TestBed.configureTestingModule({
-        //     declarations: [ AppComponent ], // declare the test component
-        //     providers: [{provide: ComponentFixtureAutoDetect, useValue: true}]
-        // });
-
         fixture = TestBed.createComponent(AppComponent);
 
-        component = fixture.componentInstance; // BannerComponent test instance
+        component = fixture.componentInstance; // AppComponent test instance
 
         // query for the title <h1> by CSS element selector
         de = fixture.debugElement.query(By.css('h1'));
-        console.log(de);
         el = de.nativeElement;
     });
 
