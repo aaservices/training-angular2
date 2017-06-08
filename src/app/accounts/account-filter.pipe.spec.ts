1c9d87bc93c8f9cec4b7cfa6d493cc09ab724248
Christina Kaskoura <christina.kaskoura@gmail.com>
2017-06-08 21:16:16 +0300

    Added unit test for account filter pipe

---
+++ src/app/accounts/account-filter.pipe.spec.ts
@@ -0,0 +1,29 @@
+import { Account } from './account.type';
+import { AccountFilterPipe } from './account-filter.pipe';
+
+describe('Account filtering based on search term', () => {
+    let accounts: Array<Account>;
+    let pipe: AccountFilterPipe;
+
+    beforeEach(() => {
+        accounts = [new Account('first', 100, true),
+            new Account('Another first', 200, true),
+            new Account('second', 100, true)];
+        pipe = new AccountFilterPipe();
+    });
+
+    it('Should return accounts matching the search term', () => {
+        let filteredAccounts = pipe.transform(accounts, 'first');
+        expect(filteredAccounts.length).toBe(2);
+    });
+
+    it('Should return all accounts if empty search term is passed', () => {
+        let filteredAccounts = pipe.transform(accounts, '');
+        expect(filteredAccounts.length).toBe(3);
+    });
+
+    it('Should return empty list if no accounts are passed', () => {
+        let filteredAccounts = pipe.transform([], 'searchTerm');
+        expect(filteredAccounts.length).toBe(0);
+    });
+});
