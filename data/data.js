a833b1476da2964b8daa410291037a08f845e0c7
Stelios Charmpalis <stelioschar@gmail.com>
2017-06-14 13:18:03 +0300

    add json-server as endpoints

---
+++ data/data.js
@@ -0,0 +1,19 @@
+let accounts = [
+    {name: 'Savings account', amount: 300, privacy: false},
+    {name: 'Current account', amount: 500, friendlyName: 'Work expenses', privacy: false},
+    {name: 'Loan', amount: -200, privacy: false},
+    {name: 'Money market account', amount: 2000, privacy: true}
+];
+
+let cards = [
+    {name: 'MasterCard', amount: 200, status: 'ACTIVE'},
+    {name: 'Visa', amount: 500, status: 'ACTIVE'},
+    {name: 'American Express', amount: -200, status: 'BANNED'},
+];
+
+module.exports = () => {
+    return {
+        accounts,
+        cards
+    }
+}
