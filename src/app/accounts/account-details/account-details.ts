9653f550b7e76a47766063a1d42fffb4c811a76e
Stelios Charmpalis <stelioschar@gmail.com>
2017-06-19 07:51:17 +0300

    add account-details:id route with snapshot params

--- src/app/accounts/account-details/account-details.ts
+++ src/app/accounts/account-details/account-details.ts
@@ -1,5 +1,7 @@
 import { Account } from './../account.type';
 import { Component, Input, OnInit, OnChanges, AfterViewInit, OnDestroy } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {AccountListService} from '../services/account-list.service';
 
 @Component({
     selector: 'account-details',
@@ -7,8 +9,20 @@ import { Component, Input, OnInit, OnChanges, AfterViewInit, OnDestroy } from '@
 })
 export class AccountDetailsComponent implements OnInit, OnChanges, AfterViewInit, OnDestroy {
     @Input() account: Account;
+    private id: string;
+    private nextId: number;
+    constructor(
+        private route: ActivatedRoute,
+        private accountService: AccountListService
+    ) {}
 
     ngOnInit() {
+        this.id = this.route.snapshot.params['id'];
+        this.accountService.getAccount(this.id)
+            .subscribe(
+                account => this.account = account
+            );
+        this.nextId = parseInt(this.id, 0);
         console.log('on init');
     }
 
--- src/app/cards/card.type.ts
+++ src/app/cards/card.type.ts
@@ -1,5 +1,6 @@
 export class Card {
     constructor(
+        public id: string,
         public name: string,
         public amount: number,
         public status: string
